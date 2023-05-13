a826cbc4ca0bebcb2ce6e02fdd125a5b88eaae4e
mpoplavkov <mihail.poplavkov@gmail.com>
2023-05-13 13:59:13 +0100

    fix: ios headers

--- packages/flutter_fuelet_contracts/ios/Classes/frb.h
+++ packages/flutter_fuelet_contracts/ios/Classes/frb.h
@@ -18,6 +18,11 @@ typedef struct wire_TokenContract {
   struct wire_uint_8_list *node_url;
 } wire_TokenContract;
 
+typedef struct wire_SendCoinsPredicate {
+  struct wire_uint_8_list *node_url;
+  struct wire_uint_8_list *code;
+} wire_SendCoinsPredicate;
+
 typedef struct DartCObject *WireSyncReturn;
 
 void store_dart_post_cobject(DartPostCObjectFnType ptr);
@@ -36,6 +41,20 @@ void wire_config__method__TokenContract(int64_t port_,
                                         struct wire_TokenContract *that,
                                         struct wire_uint_8_list *contract_id);
 
+void wire_new__static_method__SendCoinsPredicate(int64_t port_,
+                                                 struct wire_uint_8_list *node_url,
+                                                 struct wire_uint_8_list *code_hex);
+
+void wire_address__method__SendCoinsPredicate(int64_t port_, struct wire_SendCoinsPredicate *that);
+
+void wire_transfer_to__method__SendCoinsPredicate(int64_t port_,
+                                                  struct wire_SendCoinsPredicate *that,
+                                                  struct wire_uint_8_list *to,
+                                                  struct wire_uint_8_list *secret,
+                                                  uint64_t amount);
+
+struct wire_SendCoinsPredicate *new_box_autoadd_send_coins_predicate_0(void);
+
 struct wire_TokenContract *new_box_autoadd_token_contract_0(void);
 
 struct wire_uint_8_list *new_uint_8_list_0(int32_t len);
@@ -46,6 +65,10 @@ static int64_t dummy_method_to_enforce_bundling(void) {
     int64_t dummy_var = 0;
     dummy_var ^= ((int64_t) (void*) wire_new__static_method__TokenContract);
     dummy_var ^= ((int64_t) (void*) wire_config__method__TokenContract);
+    dummy_var ^= ((int64_t) (void*) wire_new__static_method__SendCoinsPredicate);
+    dummy_var ^= ((int64_t) (void*) wire_address__method__SendCoinsPredicate);
+    dummy_var ^= ((int64_t) (void*) wire_transfer_to__method__SendCoinsPredicate);
+    dummy_var ^= ((int64_t) (void*) new_box_autoadd_send_coins_predicate_0);
     dummy_var ^= ((int64_t) (void*) new_box_autoadd_token_contract_0);
     dummy_var ^= ((int64_t) (void*) new_uint_8_list_0);
     dummy_var ^= ((int64_t) (void*) free_WireSyncReturn);
