78e6d9d54790d8c621c034f1289e99f639166dd3
mpoplavkov <mihail.poplavkov@gmail.com>
2023-11-03 18:48:46 +0100

    Update ts token contract

--- packages/flutter_fuelet_contracts/web/src/token_contract.ts
+++ packages/flutter_fuelet_contracts/web/src/token_contract.ts
@@ -1,42 +1,86 @@
-import { Wallet } from "fuels";
-import { TokenContractAbi__factory } from "./types";
-import { TokenInitializeConfigOutput } from "./types/TokenContractAbi";
+import {Provider, Wallet} from "fuels";
+import {TokenContractAbi, TokenContractAbi__factory} from "./types";
 
 // DO NOT USE THIS PRIVATE KEY
 // It's present here only for the purpose of reading data from blockchain, cause currently you
 // must provide an instance of wallet to interact with smart contracts
 const READ_WALLET_PRIVATE_KEY: string =
-    "e5e05a4ab2919dc01b97c90a48853fd4dfbd204e92e44327375702ab09bb184e";
+  "e5e05a4ab2919dc01b97c90a48853fd4dfbd204e92e44327375702ab09bb184e";
 
 class TokenContract {
-    async config(
-        nodeUrl: string,
-        contractId: string
-    ): Promise<TokenInitializeConfigOutput> {
-        const wallet = Wallet.fromPrivateKey(READ_WALLET_PRIVATE_KEY, nodeUrl);
-        const contractInstance = TokenContractAbi__factory.connect(
-            contractId,
-            wallet
-        );
-        const { value } = await contractInstance.functions.config().get();
-        return value;
-    }
+  private async create_contract(nodeUrl: string, contractId: string): Promise<TokenContractAbi> {
+    const provider = await Provider.create(nodeUrl);
+    const wallet = Wallet.fromPrivateKey(READ_WALLET_PRIVATE_KEY, provider);
+    return TokenContractAbi__factory.connect(
+      contractId,
+      wallet
+    );
+  }
+
+  async total_assets(
+    nodeUrl: string,
+    contractId: string
+  ): Promise<number> {
+    const contract = await this.create_contract(nodeUrl, contractId);
+    const {value} = await contract.functions.total_assets().simulate();
+    return value.toNumber();
+  }
+
+  async total_supply(
+    nodeUrl: string,
+    contractId: string,
+    assetId: string
+  ): Promise<number | undefined> {
+    const contract = await this.create_contract(nodeUrl, contractId);
+    const {value} = await contract.functions.total_supply({value: assetId}).simulate();
+    return value?.toNumber();
+  }
+
+  async name(
+    nodeUrl: string,
+    contractId: string,
+    assetId: string
+  ): Promise<string | undefined> {
+    const contract = await this.create_contract(nodeUrl, contractId);
+    const {value} = await contract.functions.name({value: assetId}).simulate();
+    return value?.toString();
+  }
+
+  async symbol(
+    nodeUrl: string,
+    contractId: string,
+    assetId: string
+  ): Promise<string | undefined> {
+    const contract = await this.create_contract(nodeUrl, contractId);
+    const {value} = await contract.functions.symbol({value: assetId}).simulate();
+    return value?.toString();
+  }
+
+  async decimals(
+    nodeUrl: string,
+    contractId: string,
+    assetId: string
+  ): Promise<number | undefined> {
+    const contract = await this.create_contract(nodeUrl, contractId);
+    const {value} = await contract.functions.decimals({value: assetId}).simulate();
+    return value;
+  }
 }
 
 function injectObject(target: object, obj: object, name: PropertyKey) {
-    Object.defineProperty(target, name, {
-        value: obj,
-        writable: false,
-        enumerable: true,
-        configurable: false,
-    });
+  Object.defineProperty(target, name, {
+    value: obj,
+    writable: false,
+    enumerable: true,
+    configurable: false,
+  });
 
-    if (typeof document !== "undefined") {
-        const loadedEvent = new CustomEvent(`${String(name)} loaded`, {
-            detail: obj,
-        });
-        document.dispatchEvent(loadedEvent);
-    }
+  if (typeof document !== "undefined") {
+    const loadedEvent = new CustomEvent(`${String(name)} loaded`, {
+      detail: obj,
+    });
+    document.dispatchEvent(loadedEvent);
+  }
 }
 
 injectObject(window, new TokenContract(), "flutter_fuelet_token_contract");
